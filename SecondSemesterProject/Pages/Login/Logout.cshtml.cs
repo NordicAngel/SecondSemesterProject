58e2d836c342f3947458a7f89f32ede78972d57e
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-04-27 14:12:32 +0200

    Login Update

--- SecondSemesterProject/Pages/Login/Logout.cshtml.cs
+++ SecondSemesterProject/Pages/Login/Logout.cshtml.cs
@@ -4,13 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SecondSemesterProject.Interfaces;
 
 namespace SecondSemesterProject.Pages.Login
 {
     public class LogoutModel : PageModel
     {
-        public void OnGet()
+        private IMemberService MemberService;
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public IMember Member { get; set; }
+
+        public string InfoText;
+
+        public LogoutModel(IMemberService service)
         {
+            MemberService = service;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            return Page();
         }
     }
 }
