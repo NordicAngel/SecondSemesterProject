cab0743911f853550b577efce7665729cdf1e2cb
emboni <93706027+emboni@users.noreply.github.com>
2022-04-27 10:55:28 +0200

    Added SQL Connection

---
+++ SecondSemesterProject/Services/EventService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SecondSemesterProject.Interfaces;
+using SecondSemesterProject.Models;
+
+namespace SecondSemesterProject.Services
+{
+    public class EventService : IEventService
+    {
+        public void CreateEventAsync(Event ev)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event GetEventAsync(int evId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> GetAllEventAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEventAsync(int evId, Event ev)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEventAsync(int evId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
