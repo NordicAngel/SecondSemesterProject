7ad5d1a710a6c57992085c8125b6a67af9e95c44
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-05-03 13:16:21 +0200

    Family Group

--- SecondSemesterProject/Services/MemberService.cs
+++ SecondSemesterProject/Services/MemberService.cs
@@ -16,14 +16,15 @@ namespace SecondSemesterProject.Services
         private string selectByFamilyGroupIdSql = $"select * from {databaseName} where FamilyGroupId = @ID";
         private string selectByNameSql = $"select * from {databaseName} where Name like @Name";
 
-        private string insertSql = $"insert into {databaseName} Values (NULL, @Name, @Email, @Password, @PhoneNumber, @BoardMember, @HygieneCertified)";
-        private string updateSql = $"update {databaseName} set Name = @Name, Email = @Email, Password = @Password, PhoneNumber = @PhoneNumber, BoardMember = @BoardMember, HygieneCertified = @HygieneCertified where Id = @ID";
+        private string insertSql = $"insert into {databaseName} values (NULL, @Name, @Email, @Password, @PhoneNumber, @BoardMember, @HygieneCertified)";
+        private string updateSql = $"update {databaseName} set FamilyGroupId = @FamilyGroupId, Name = @Name, Email = @Email, Password = @Password, PhoneNumber = @PhoneNumber, BoardMember = @BoardMember, HygieneCertified = @HygieneCertified where Id = @ID";
         private string deleteSql = $"delete from {databaseName} where Id = @ID";
 
         private string loginSql = $"select * from {databaseName} where Email = @Email, Password = @Password";
 
         private string selectFamilyGroupSql = "select * from JO22_FamilyGroup";
-        private string insertFamilyGroupSql = "insert into JO22_FamilyGroup";
+        private string selectFamilyGroupIdSql = "select max(Id) from JO22_FamilyGroup";
+        private string insertFamilyGroupSql = "insert into JO22_FamilyGroup default values";
 
         public MemberService(IConfiguration configuration) : base(configuration)
         {
@@ -65,6 +66,7 @@ namespace SecondSemesterProject.Services
                 {
                     SqlCommand command = new SqlCommand(updateSql, connection);
                     command.Parameters.AddWithValue("@ID", id);
+                    command.Parameters.AddWithValue("@FamilyGroupId", member.FamilyGroupID);
                     command.Parameters.AddWithValue("@Name", member.Name);
                     command.Parameters.AddWithValue("@Email", member.Email);
                     command.Parameters.AddWithValue("@Password", member.Password);
@@ -164,15 +166,33 @@ namespace SecondSemesterProject.Services
             {
                 try
                 {
-                    SqlCommand command = new SqlCommand(insertFamilyGroupSql, connection);
-                    command.Connection.Open();
-                    command.ExecuteNonQuery();
+                    SqlCommand command_1 = new SqlCommand(insertFamilyGroupSql, connection);
+                    command_1.Connection.Open();
+                    command_1.ExecuteNonQuery();
+                }
+                catch (SqlException)
+                {
+                    throw;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    SqlCommand command_2 = new SqlCommand(selectFamilyGroupIdSql, connection);
+                    command_2.Connection.Open();
+                    int familyGroupId = command_2.ExecuteNonQuery();
 
                     foreach (IMember member in members)
                     {
-                        member.FamilyGroupID = 0;
+                        member.FamilyGroupID = familyGroupId;
 
-                        // Update members with new family group id
+                        UpdateMember(member.ID, member);
                     }
                 }
                 catch (SqlException)
